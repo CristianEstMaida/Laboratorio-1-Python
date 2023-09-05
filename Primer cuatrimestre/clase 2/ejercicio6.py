e91d73d5da9f0a8624cc96be90f037254521a011
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2023-09-05 19:28:21 -0300

    Update ejercicio6.py

--- Primer cuatrimestre/clase 2/ejercicio6.py
+++ Primer cuatrimestre/clase 2/ejercicio6.py
@@ -1,13 +1,10 @@
 lista_numeros = [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
 
-mayor = lista_numeros[0]  # Suponemos que el primer número es el mayor
+mayor = None
 
 for numero in lista_numeros:
-    if numero > mayor:
+    if mayor == None or numero > mayor:
         mayor = numero
 
 print("El número mayor en la lista es:", mayor)
-
-
-
 
